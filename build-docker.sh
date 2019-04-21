50a3cc2328c24296dbcb2d02419889b83cc7c664
Stefan Schneider <schneider@hrz.uni-marburg.de>
2019-04-21 18:40:48 +0200

    add build-docker.sh for locally build within docker image

---
+++ build-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker run --privileged \
+    -it \
+    --rm \
+    --name "bootlinux-cont" \
+    --mount type=bind,source="$(pwd)",target=/build \
+    registry.gitlab.com/eqsoft/bootlinux-docker:latest \
+    /bin/bash -c 'cd /build; ./build.sh'
